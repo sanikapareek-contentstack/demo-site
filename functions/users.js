692a983e61575e0ef5d97b15ee7a726d82f6f5e0
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2024-01-24 20:58:22 +0530

    Add cloud functions

---
+++ functions/users.js
@@ -0,0 +1,9 @@
+export default function users(request, response){
+  const users= [
+    {name: 'Sanika', age: '22'},
+    {name: 'Siddhi', age:'22'},
+    {name: 'Shravani', age: '22'}
+  ];
+
+  response.status(200).send(users);
+}
