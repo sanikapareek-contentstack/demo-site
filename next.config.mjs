86bcd4106b3bac6ba14a0b2811908779af516a6e
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2024-08-07 12:05:23 +0530

    Add generateBuildId in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  generateBuildId: () => {    
+    return  process.env.CONTENTSTACK_LAUNCH_DEPLOYMENT_UID
+  },
 };
 
 export default nextConfig;
