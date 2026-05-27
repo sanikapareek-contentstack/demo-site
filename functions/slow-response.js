b43c04015e16f58e8c2bbba6f5828d70700d3172
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2026-05-27 15:16:08 +0530

    Fix slow-response handler to actually await the 35s delay

--- functions/slow-response.js
+++ functions/slow-response.js
@@ -1,8 +1,7 @@
-export default function handler(request, response) {
-  setTimeout(() => {
-    response.status(200).send({
-      message: 'Response after 35 seconds delay',
-      timestamp: new Date().toISOString(),
-    });
-  }, 35000);
+export default async function handler(request, response) {
+  await new Promise((resolve) => setTimeout(resolve, 35000));
+  response.status(200).send({
+    message: 'Response after 35 seconds delay',
+    timestamp: new Date().toISOString(),
+  });
 }
