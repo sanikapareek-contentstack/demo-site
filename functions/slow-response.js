fb1f86c6a574350072bce360b48004bfb838a752
Sanika Pareek <sanika.pareek@contentstack.com>
2026-08-20 18:58:59 +0530

    Refactor response handling in slow-response.js

--- functions/slow-response.js
+++ functions/slow-response.js
@@ -1,10 +1,8 @@
 export default async function handler(request, response) {
   await new Promise((resolve) => setTimeout(resolve, 35000));
-  response
-    .status(200)
-    .set('Cache-Control', 'no-cache, no-store, s-maxage=0')
-    .send({
-      message: 'Response after 35 seconds delay',
-      timestamp: new Date().toISOString(),
-    });
+  response.setHeader('Cache-Control', 'no-cache, no-store, s-maxage=0');
+  response.status(200).json({
+    message: 'Response after 35 seconds delay',
+    timestamp: new Date().toISOString(),
+  });
 }
