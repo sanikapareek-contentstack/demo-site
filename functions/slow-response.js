62cacb593be76bdf41ff6e9bd235f9ea645541b3
Sanika Pareek <sanika.pareek@contentstack.com>
2026-08-20 19:05:27 +0530

    Increase delay in slow-response handler to 100 seconds
    Increased the delay in the slow-response handler from 35 seconds to 100 seconds.

--- functions/slow-response.js
+++ functions/slow-response.js
@@ -1,5 +1,5 @@
 export default async function handler(request, response) {
-  await new Promise((resolve) => setTimeout(resolve, 35000));
+  await new Promise((resolve) => setTimeout(resolve, 100000));
   response.setHeader('Cache-Control', 'no-cache, no-store, s-maxage=0');
   response.status(200).json({
     message: 'Response after 35 seconds delay',
